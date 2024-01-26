58f50de98ce5d7e7b4aba28e52b74931d054fcb2
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2024-01-25 21:11:39 -0300

    Add files via upload

---
+++ logica-js/js-curso-2-desafios_4/desafios.js
@@ -0,0 +1,12 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+console.log(linguagensDeProgramacao);
+
+let novaLista = ['Alex', 'Joe', 'Crystal'];
+console.log(novaLista[0]);
+console.log(novaLista[1]);
+console.log(novaLista[novaLista.length - 1]);
